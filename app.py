69948a63870c59a9b8c34e437e0dc12e37ec080b
Alfredo Ocegueda <68208064+AlfredoOcegueda@users.noreply.github.com>
2021-01-18 12:45:14 -0500

    user inputs example

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 #Imports
-from flask import Flask, render_template, redirect, jsonify
+import numpy as np
+from flask import Flask, render_template, redirect, jsonify, request
+
+
 
 #Flask Setup
 app = Flask(__name__)
@@ -10,9 +13,23 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/credit")
+@app.route('/credit')
 def credit():
-    return render_template("credit.html")
+    return render_template('credit.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+
+    # retrieving values from form
+    init_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(init_features)]
+
+    #prediction = model.predict(final_features) # making prediction
+
+    return render_template('credit.html', prediction_text='Predicted Class: {}'.format(final_features)) # rendering the predicted result
 
 if __name__ == '__main__':
     app.run(debug=True)
